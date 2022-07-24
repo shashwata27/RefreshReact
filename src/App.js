12f048ee5b94d0fed97b88ebc52b20da3900dcb3
Shashwata Saha <ShashwataSaha@gmail.com>
2022-07-24 14:18:20 +0530

    [shashwata] added filter functionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import { nanoid } from "nanoid";
 import { useState } from "react";
-import { toHaveStyle } from "@testing-library/jest-dom/dist/matchers";
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
 
   const tasksNoun = tasks.length !== 1 ? "tasks" : "task";
   const headingText = `${tasks.length} ${tasksNoun} remaining`;
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
 
   const addTask = (name) => {
     const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
@@ -47,18 +64,14 @@ function App(props) {
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
+      <div className="filters btn-group stack-exception">{filterList}</div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        {tasks.map((task) => {
+        {tasks.filter(FILTER_MAP[filter]).map((task) => {
           return (
             <Todo
               id={task.id}
